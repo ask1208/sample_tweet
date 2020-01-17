81e8de6acdca5a1ba233af0d9f641449b0597eeb
ask1208 <t.ask.128@icloud.com>
2020-01-17 22:45:00 +0900

    編集機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,6 +21,10 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
 
 
   private
